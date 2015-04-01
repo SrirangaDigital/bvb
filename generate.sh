9bacef6495652e645bdcd1d2168b8aee56439aa8
suresh07 <soori_rozee@yahoo.com>
2015-04-01 18:54:05 +0530

    Commit No. 5 - testocr and searchtable added to database

--- generate.sh
+++ generate.sh
@@ -5,9 +5,13 @@ db="bvb"
 usr="root"
 pwd="mysql"
 
-echo "drop database if exists bvb; create database bvb;" | /usr/bin/mysql -uroot -pmysql
+echo "drop database if exists bvb; create database bvb;" | /usr/bin/mysql -uroot -p$pwd
 
 perl insert_author.pl $host $db $usr $pwd
 perl insert_feat.pl $host $db $usr $pwd
 perl insert_series.pl $host $db $usr $pwd
 perl insert_articles.pl $host $db $usr $pwd
+perl ocr.pl $host $db $usr $pwd
+perl searchtable.pl $host $db $usr $pwd
+
+echo "create fulltext index text_index on searchtable (text);" | /usr/bin/mysql -uroot -pmysql $db
