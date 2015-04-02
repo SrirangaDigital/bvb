5cc7b40773a4ded12613b8ab68a0062281afe582
suresh07 <soori_rozee@yahoo.com>
2015-04-02 17:54:44 +0530

    Commit No. 7 - generate.sh changed - db and pwd parametrized

--- generate.sh
+++ generate.sh
@@ -5,7 +5,7 @@ db="bvb"
 usr="root"
 pwd="mysql"
 
-echo "drop database if exists bvb; create database bvb;" | /usr/bin/mysql -uroot -p$pwd
+echo "CREATE DATABASE IF NOT EXISTS $db CHARACTER SET utf8 COLLATE utf8_general_ci;" | /usr/bin/mysql -uroot -p$pwd
 
 perl insert_author.pl $host $db $usr $pwd
 perl insert_feat.pl $host $db $usr $pwd
@@ -14,4 +14,4 @@ perl insert_articles.pl $host $db $usr $pwd
 perl ocr.pl $host $db $usr $pwd
 perl searchtable.pl $host $db $usr $pwd
 
-echo "create fulltext index text_index on searchtable (text);" | /usr/bin/mysql -uroot -pmysql $db
+echo "create fulltext index text_index on searchtable (text);" | /usr/bin/mysql -uroot -p$pwd $db
